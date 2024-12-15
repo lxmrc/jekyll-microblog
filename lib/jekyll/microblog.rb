c9299d666167bfddd1cd4bae5ac328547c7544aa
Alex Mercea <alex@lxmrc.com>
2024-12-15 15:43:28 +1100

    Initialize repo

---
+++ lib/jekyll/microblog.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "microblog/version"
+
+module Jekyll
+  module Microblog
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
